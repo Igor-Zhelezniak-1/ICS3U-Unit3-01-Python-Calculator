c473b5d7a6ff37bc3d6f9c9153d824eaec20f821
Igor <ihor.zhelezniak@mths.ca>
2021-09-16 16:08:19 +0000

    aded calculator

---
+++ calculator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# Created by: Igor
+# Created on: Sept 2021
+# This is calculator
+
+
+def main():
+    # This is calculator
+    # input
+
+    first = int(input("Enter first number: "))
+    second = int(input("Enter second number: "))
+
+    # process
+    total = first + second
+
+    # output
+    print("{0} + {1} = {2}".format(first, second, total))
+
+
+if __name__ == "__main__":
+    main()
